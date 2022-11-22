a17054edcf805a9e2252f1690079fc495f6ec979
erik_s09 <septianaerik45@gmail.com>
2022-11-22 12:06:37 +0700

    edit composer.json

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -42,7 +42,7 @@
     <div class="container d-flex justify-content-between">
 
       <div class="logo">
-        <h1><a href="/"><span>e</span>Business</a></h1>
+        <h1><a href="/"><span>AP</span>WI</a></h1>
         <!-- Uncomment below if you prefer to use an image logo -->
         <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
       </div>
