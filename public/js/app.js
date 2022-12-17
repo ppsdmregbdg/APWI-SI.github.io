b272ed43f3ad8176144b7d7f20490c87c4b71769
erik_s09 <septianaerik45@gmail.com>
2022-12-17 11:17:53 +0700

    fix

--- public/js/app.js
+++ public/js/app.js
@@ -19437,8 +19437,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\APWI-SI\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\APWI-SI\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! C:\xampp\htdocs\apwi-si\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\apwi-si\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
